260463fce9fca649d1d3b6ca4be25cf95a21e541
Jeffrey Yurkiw <jyurkiw@yahoo.com>
2016-11-23 01:05:27 -0800

    Added linq.js. Added logic to clean up the _connectionPhase collection in the game loop.

--- game/red-mud-server.js
+++ game/red-mud-server.js
@@ -10,7 +10,9 @@ function RedMUDServer(httpServer) {
     var io = require('socket.io')(httpServer);
     var ccodes = require('../util/confirmation-codes');
     var conf = require('../config/conf');
+    var linq = require('linq');
     var gameloop = require('node-gameloop');
+
     var loopid = null;
 
     var _connectionPhase = {};
@@ -55,18 +57,41 @@ function RedMUDServer(httpServer) {
                 character: {}
             };
             _commandPhase.push(username);
-            delete _connectionPhase[socket.id];
+            _connectionPhase[socket.id].verified = true;
         } else {
             socket.emit('verify', false);
         }
     }
 
+    /**
+     * Start the game loop.
+     * 
+     * @memberof red-mud-server
+     */
     function start() {
         loopid = gameloop.setGameLoop(function(delta) {
             console.log('in the mud loop');
+            connectionPhaseHandler();
         }, conf.turnDuration);
     }
 
+    function connectionPhaseHandler() {
+        Object.keys(_connectionPhase).forEach(function(id) {
+            var conn = _connectionPhase[id];
+            if (Date.now() > conn.expiery && !conn.verified) {
+                conn.socket.disconnect(0);
+                conn.socket.removeListener('verify', verifyConnection);
+            }
+        });
+
+        _connectionPhase = linq.from(_connectionPhase).where(function(conn) { return !conn.disconnected; }).toArray();
+    }
+
+    /**
+     * Stop the game loop.
+     * 
+     * @memberof red-mud-server
+     */
     function stop() {
         if (loopid !== null) {
             gameloop.clearGameLoop(loopid);
@@ -74,11 +99,17 @@ function RedMUDServer(httpServer) {
         }
     }
 
+    // Socket connection event handler.
+    // Add the socket to the connection phase
+    // collection with a 10 second expiery
+    // and register the verify event for connection
+    // verification.
     io.on('connection', function(socket) {
         console.log(socket.id + ' connected.');
         _connectionPhase[socket.id] = {
             socket: socket,
-            expiery: Date.now() + 10000
+            expiery: Date.now() + 10000,
+            verified: false
         };
 
         socket.on('verify', verifyConnection);
